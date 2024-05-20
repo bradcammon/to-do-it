8e5c163de7b7daa495b2ceea35294519abb559ea
Brad Cammon <brad.cammon@gmail.com>
2024-05-20 11:18:39 -0700

    trying to get emit from todolist item working...will probably change this later

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,13 +2,22 @@
 import ToDoListItem from './ToDoListItem.vue'
 import { useToDoStore } from '@/stores/ToDoStore'
 
+// import { ref } from 'vue'
+// let childMsg = ref('No child msg yet')
+
 let toDo = useToDoStore()
+
+//TODO: need to know what item this is...
+function handleItemEdit(newSubject) {
+  console.log('new:', newSubject)
+}
 </script>
 
 <template>
   <ul>
     <ToDoListItem
       v-for="item in toDo.items"
+      @response="handleItemEdit"
       :key="item.id"
       :name="item.name"
       :id="item.id"
--- src/components/ToDoListItem.vue
+++ src/components/ToDoListItem.vue
@@ -4,11 +4,19 @@ import { useToDoStore } from '@/stores/ToDoStore'
 let toDo = useToDoStore()
 
 // import { ref } from 'vue'
+
 defineProps({
   name: String,
   id: String,
   item: Object
 })
+
+const emit = defineEmits(['response'])
+
+// emit('response', 'hello from child')
+
+// const itemName = ref(item)
+
 //TODO: Move this logic to the Store?
 function toggleComplete(item) {
   item.completed = !item.completed
@@ -21,12 +29,24 @@ function deleteItem(item) {
   // Update the api
   toDo.deleteItem(item)
 }
+
+function textEdited(e) {
+  console.log(e)
+  // alert('edited')
+  const form = new FormData(e.target)
+  const name = form.get('editedName')
+  // console.log(name)
+  emit('response', name)
+}
 </script>
 
 <template>
   <li :class="{ completed: item.completed }">
     <input type="checkbox" @click="toggleComplete(item)" />{{ item.name }}
     <button @click="deleteItem(item)" id="delete">Delete</button>
+    <form @submit.prevent="textEdited">
+      <input type="text" :value="item.name" name="editedName" />
+    </form>
   </li>
 </template>
 

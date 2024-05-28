bb4a1c99b6f1502119b763b3d51aa71bc8e1ea81
Brad Cammon <brad.cammon@gmail.com>
2024-05-27 22:25:48 -0700

    Fixed alignment of list items

--- src/components/ToDoListItem.vue
+++ src/components/ToDoListItem.vue
@@ -36,26 +36,41 @@ function submitEdit(item) {
 </script>
 
 <template>
-  <v-list-item>
-    <li v-if="!editMode" :class="{ completed: item.completed }">
-      <input type="checkbox" @click="toggleComplete(item)" />
-      {{ item.name }}
-      <v-btn @click="editItem()" density="comfortable" icon="mdi-pencil" size="small"></v-btn>
-      <v-btn @click="deleteItem(item)" density="comfortable" icon="mdi-delete" size="small"></v-btn>
-    </li>
-    <div v-if="editMode">
-      <v-text-field
-        label="Enter ToDo Item"
-        v-model="inputName"
-        @keyup.enter="submitEdit(item)"
-        variant="outlined"
-      ></v-text-field>
-      <v-btn @click="submitEdit(item)" density="comfortable" icon="mdi-check" size="small"></v-btn>
-    </div>
-  </v-list-item>
+  <li v-if="!editMode" class="listItem" :class="{ completed: item.completed }">
+    <input class="listItemCheckbox" type="checkbox" @click="toggleComplete(item)" />
+    {{ item.name }}
+    <v-btn @click="editItem()" density="comfortable" icon="mdi-pencil" size="small"></v-btn>
+    <v-btn @click="deleteItem(item)" density="comfortable" icon="mdi-delete" size="small"></v-btn>
+  </li>
+  <div class="editMode" v-else>
+    <div></div>
+    <v-text-field
+      density="compact"
+      v-model="inputName"
+      @keyup.enter="submitEdit(item)"
+      variant="outlined"
+    ></v-text-field>
+
+    <v-btn @click="submitEdit(item)" density="comfortable" icon="mdi-check" size="small"></v-btn>
+  </div>
 </template>
 
 <style>
+.listItemCheckbox {
+  justify-self: center;
+  width: 1em;
+}
+.listItem {
+  display: grid;
+  grid-template-columns: 0.5fr 9.5fr 1fr 1fr;
+  column-gap: 0.5em;
+  margin: 5px 0px;
+}
+.editMode {
+  display: grid;
+  grid-template-columns: 0.5fr 9.5fr 1fr 1fr;
+  column-gap: 0.5em;
+}
 .notCompleted {
   color: red;
 }
